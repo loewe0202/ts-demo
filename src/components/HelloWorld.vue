90b729b63df3a6ccc481f06674f7e141b9d1aced
linzhuoming <linzhuoming@myoas.com>
2020-01-09 17:59:11 +0800

    vue ts-demo

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    name: {{ name }} ···==··· age: {{ age }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+
+  age = 26;
+  name = 'hello word!';
+
+  created() {
+    this.alertName();
+  }
+
+  alertName(): void {
+    console.log(this.name + this.msg);
+  }
+}
+</script>
+
+<style scoped lang="scss"></style>
